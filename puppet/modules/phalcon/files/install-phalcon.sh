2f680a82a965e2c36e8238cb0879de79d0ae1d6f
Angel Psiakis <angel.psiakis@dxi.eu>
2013-06-26 15:36:31 +0100

    Install phalcon devtools after installing the phalcon extension

--- puppet/modules/phalcon/files/install-phalcon.sh
+++ puppet/modules/phalcon/files/install-phalcon.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+cd /tmp
+
 git clone git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
 sudo ./install
+
+cd
+
+git clone git://github.com/phalcon/phalcon-devtools.git
+cd phalcon-devtools
+sudo ./phalcon.sh
+
